2a1cba634c938de5d7fe428fcb96e2d44766b940
AYAELALAMI <eaya4570@gmail.com>
2024-01-30 02:35:36 -0800

    printfp

---
+++ string_handler.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * string_holder - prints strings
+ * @str: holds and prints the string
+ * Return: the string
+ */
+int string_holder(char *str)
+{
+	int n = 0;
+	int count = 0;
+
+	if (str == NULL)
+	{
+		char *prnull = "(null)";
+
+		while (n <= 5)
+		{
+		_putchar(prnull[n]);
+		n++;
+		count++;
+		}
+	return (count);
+	}
+	else
+	{
+		while (*str != '\0')
+		{
+			_putchar(*str);
+			count++;
+			str++;
+		}
+	}
+	return (count);
+}
