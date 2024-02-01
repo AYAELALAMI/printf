3cc957f2310c5dfc8b74053f049613c508602b70
AYAELALAMI <eaya4570@gmail.com>
2024-02-01 01:11:26 -0800

    printfp

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _printf - function as printf that prints format's characters
+ * @format: includes characters and specifiers
+ * Return: count the length of the format
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list argu_ments;
+
+	va_start(argu_ments, format);
+	if (!format)
+	{
+		return (-1);
+	}
+		while (*format)
+		{
+			if (format[0] == '%' && format[1] != '\0')
+			{
+				format++;
+				switch (*format)
+				{
+					case 's':
+						count += string_handler(va_arg(argu_ments, char*);
+						break;
+					case 'c':
+						count += _putchar(va_arg(argu_ments, int));
+						break;
+					case '%':
+						count += _putchar(37);
+						break;
+					default:
+						count += _putchar(37));
+						count += _putchar(*format);
+						break;
+						}
+						}
+			else
+			{
+				count += _putchar(*format);
+			}
+			format++;
+		}
+		va_end(argu_ments);
+		return (count);
+}
---
+++ numbers_handler.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * numbers_handler - return the count
+ * @num: int
+ * Return: count
+ */
+
+int numbers_handler(int num)
+{
