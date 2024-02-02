3123799a66e57efd2f3fea6d37b6b80b6cb8335b
AYAELALAMI <eaya4570@gmail.com>
2024-02-02 04:11:50 -0800

    printfp

--- _len_printer.c
+++ _len_printer.c
@@ -1,14 +1,24 @@
 #include "main.h"
 /**
- * _len_printer -  print the string and its length
- * @str: the string
+ * _len_printer -  print the string and count its length
+ * @strr: the string
  * Return: the count of the string
  */
-int _len_printer(char *str)
+int _len_printer(char *strr)
 {
-	int len;
+	int len = strlen(strr);
+	int n = 0;
+	int count = 0;
 
-	for (len = 0; str[len] != '\0'; len++)
-	return (len);
+	if (strr == NULL)
+	{
+		exit(-1);
+	}
+
+	for (n = 0; n < len; n++)
+	{
+		count += _putchar(strr[n]);
+	}
+	return (count);
 }
 
